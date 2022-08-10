e4b66448074f7ab81be8522354eae5cbdb35962d
Busu <busurmanbek000@gmail.com>
2022-08-10 14:04:50 +0600

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Class c = new Class(25,"Word",new int[]{1,2,3,4,5});
+        System.out.println(c.toString());
     }
 }
