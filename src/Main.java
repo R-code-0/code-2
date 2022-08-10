4d451196e3d753c7da92c9cbea04c82b55011577
Busu <busurmanbek000@gmail.com>
2022-08-10 14:07:24 +0600

    ..

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Class c = new Class(25,"Word",new int[]{1,2,3,4,5});
-        System.out.println(c.toString());
+        Class c = new Class(25, "Word", new int[]{1, 2, 3, 4, 5});
+        System.out.println(c.toString());//1
+        System.out.println("Number: " + c.getNumber() + ", word: " + c.getWord() + ", Array: " + Arrays.toString(c.getArray()));//2
     }
 }
